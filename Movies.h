6d31304af02f48ec00edf602bb18b6624b57b743
ihages <hagemaij@mail.uc.edu>
2025-02-19 16:17:30 -0500

    Update Movies.h

--- Movies.h
+++ Movies.h
@@ -1,30 +1,24 @@
 #ifndef __MOVIES_HPP
 #define __MOVIES_HPP
-
-#include "../CS2028Lab4/Shows.h"
 #include <iostream>
 
 class Movies {
-    private:
-        int runtime; //Total length of the movie in minutes
+private:
+    int runtime; //Total length of the movie in minutes
 
-    public:
-        //Getters
-        int getRunTime(){ return runtime; }
+public:
+    //Getters
+    int getRunTime() const { return runtime; }
 
-        //Setters
-        void setRunTime(int newRunTime){ runtime = newRunTime; }
+    //Setters
+    void setRunTime(int newRunTime) { runtime = newRunTime; }
 
-        //Constructors
-        Movies() : runtime(0) {}   //Default Constructor
+    //Constructors
+    Movies() : runtime(0) {}   //Default Constructor
 
-        Movies(std::string title, std::string desc, int newRunTime) : runtime(newRunTime) {}
-
-        //Functions
-        void Play();
+    //Functions
+    void const Play();
 
 };
-
-
 
 #endif
