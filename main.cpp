a217d59913cc310e9bf16723257de918bf2c2005
ihages <hagemaij@mail.uc.edu>
2025-02-22 13:54:27 -0500

    Updated main to tell users that they cannot add movies when shelf is full and cannot remove movies when the shelf is empty

--- main.cpp
+++ main.cpp
@@ -8,24 +8,35 @@ int main() {
 	
 	Shelf shelf;
 	while (true) {
-		std::cout << "1. Add a movie to the shelf\n2. Remove a movie from the shelf\n3. See how many movies are currently on the shelf\n4. Quit" << std::endl;
+		std::cout << "\n1. Add a movie to the shelf\n2. Remove a movie from the shelf\n3. See how many movies are currently on the shelf\n4. Quit" << std::endl;
 		int opChoice = 0;
 		
 		opChoice = 0;
 		std::cin >> opChoice;
 		if (opChoice == 1) {
 			//addmovie
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Igitnore '\n' for getline
-			std::cout << "\nWhat is the title of the movie you would like to add?" << std::endl;
-			std::string title;
-			std::getline(std::cin,title);
-			Movies *movie = new Movies(title);	//Dynamically create a new movie
-			shelf.addMovie(*movie);
-			std::cout << title << " was added to the shelf." << std::endl;
+			if (shelf.getIndex() < SIZE) {
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Igitnore '\n' for getline
+				std::cout << "\nWhat is the title of the movie you would like to add?" << std::endl;
+				std::string title;
+				std::getline(std::cin,title);
+				Movies *movie = new Movies(title);	//Dynamically create a new movie
+				shelf.addMovie(*movie);
+				std::cout << title << " was added to the shelf." << std::endl;
+			}
+			else {
+				std::cout << "\nThere is not enough room on the shelf. Please remove a movie before adding a new one." << std::endl;
+			}
+
 		}
 		else if (opChoice == 2) {
 			//removemovie
-			std::cout << "\nRemoved " << shelf.remMovie().getTitle() << " from the shelf." << std::endl;
+			if (shelf.getIndex() > 0) {
+				std::cout << "\nRemoved " << shelf.remMovie().getTitle() << " from the shelf." << std::endl;
+			} else {
+				std::cout << "There are no movies on the shelf. Please add a movie if you want to remove a movie" << std::endl;
+			}
+			
 		}
 		else if (opChoice == 3) {
 			//countmovies
@@ -36,7 +47,7 @@ int main() {
 			return 0;	
 		}
 		else {
-			std::cout << "Invalid input. Please choose a number from the list." << std::endl;
+			std::cout << "\nInvalid input. Please choose a number from the list." << std::endl;
 		}
 	}
 	return 0;
